66165d57a4c1787137eb3dfa43986d9ec4b19e8a
test <82846064+TriLinder@users.noreply.github.com>
2023-03-25 11:49:15 +0100

    Kolomanager a popup

--- pvs/Assets/KytkaPopUp.cs
+++ pvs/Assets/KytkaPopUp.cs
@@ -2,19 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class KytkaPopUp : MonoBehaviour
 {
     public GameObject kytka;
-    public Text typkytkytext;
-    public Text popiskytkytext;
+    public TMP_Text typkytkytext;
+    public TMP_Text popiskytkytext;
     public Image obrazekkytkyimage;
     public void Pop(string typkytky, string popiskytky, Texture2D obrazekkytky)
     {
-        kytka.SetActive(true)
+        kytka.SetActive(true);
         typkytkytext.text = typkytky;
         popiskytkytext.text = popiskytky;
-        obrazekkytkyimage.mainTexture = obrazekkytky;
+        
     }
 
 }
