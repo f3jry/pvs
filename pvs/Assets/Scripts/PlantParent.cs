683d3b6109bab0ce766968d8949c19ab7db244da
kkumaaa-klarka <kumawoa@gmail.com>
2023-03-25 17:38:09 +0100

    pridany druhy kytek

--- pvs/Assets/Scripts/PlantParent.cs
+++ pvs/Assets/Scripts/PlantParent.cs
@@ -7,5 +7,6 @@ public class PlantParent : ScriptableObject
     public string Name;
     public string AbilityDescription;
     public AbilityBase BaseAbility;
-    public int Price;
+    public int PriceSell;
+    public int PriceBuy;
 }
