6e87eeed0954cb7229cd78356337a44951b7319c
Kobliznik <jakub.varga06@gmail.com>
2023-03-26 15:24:25 +0200

    Ability a tak

--- pvs/Assets/Plants/Scripts/Potato.cs
+++ pvs/Assets/Plants/Scripts/Potato.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jablko : AbilityBase
+public class Potato : AbilityBase
 {
 
     private void Awake()
